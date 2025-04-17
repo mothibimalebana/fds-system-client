e0d4836d370a9a7b07bd58c493e7dfc83dd69ce2
mothibimalebana <mothibi.mm12@gmail.com>
2025-04-17 09:26:42 +0200

    created dashboard container

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,17 +1,7 @@
-const Cards ({user}) {
+const Dashboard = () => {
     return(
-        <div className="card-container">
-            <div className="top">
-                <div className="balance"></div>
-                <div className="rcs"></div>
-            </div>
-            <div className="middle">
-                <div className="cardHolder"></div>
-                <div className="validDate"></div>
-            </div>
-            <div className="bottom">
-                <div className="cardNumber"></div>
-            </div>
+        <div className="dashboard-container">
+            <div className="header"></div>
         </div>
     )
 }
