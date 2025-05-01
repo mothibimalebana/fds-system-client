3340bf168c0a1df00dc118b7168118b9309920b0
mothibimalebana <mothibi.mm12@gmail.com>
2025-05-01 21:30:55 +0200

    upd: install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from './components/nav/Nav'
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
+import Dashboard from './dashboard/Dashboard';
 
+let router = createBrowserRouter([
+  {
+    path: '/',
+    Component: <Dashboard/>
+  }
+])
 function App() {
 
   return (
